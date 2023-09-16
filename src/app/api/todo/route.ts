6e0a2e3bdef8979d32b44d4743711cbfe7a80df6
Umair <mrumairhassan@outlook.com>
2023-09-16 18:33:14 +0500

    POST request is added and data added successfully

--- src/app/api/todo/route.ts
+++ src/app/api/todo/route.ts
@@ -2,16 +2,38 @@ import { db } from '@vercel/postgres';
 import { NextRequest, NextResponse } from "next/server";
 
 
-export async function GET( request: NextRequest){
-const client = await db.connect();
+export async function GET(request: NextRequest) {
+    const client = await db.connect();
 
 
-try {
-    await client.sql `CREATE TABLE IF NOT EXISTS Todos(id serial, Task varchar(255));`
-    return NextResponse.json({message: "you call this api"})
-} catch(err){
-console.log(err);
-return NextResponse.json({message: "Some thing went wrong"})
+    try {
+        await client.sql`CREATE TABLE IF NOT EXISTS Todos(id serial, Task varchar(255));`
+        return NextResponse.json({ message: "you call this api" })
+    } catch (err) {
+        console.log(err);
+        return NextResponse.json({ message: "Some thing went wrong" })
 
+    }
+}
+
+
+
+
+export async function POST(request:NextRequest){
+
+    const client = await db.connect();
+    const req = await request.json();
+try{
+if(req.task){
+await client.sql`INSERT INTO Todos(Task) VALUES(${req.task})`
+return NextResponse.json({message: "Data added successfully"})
+
+}else{
+    throw new Error("TASK REQUIRED")
+}
+}
+catch(error){
+    console.log(error);
+    return NextResponse.json({ message: (error as {message : string}).message})
 }
 }
