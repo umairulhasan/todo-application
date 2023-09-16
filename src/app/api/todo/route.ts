5231a6abf975b935f1cdf2521b24432c5820db5e
Umair <mrumairhassan@outlook.com>
2023-09-16 17:16:26 +0500

    Try catch or error handelling added

--- src/app/api/todo/route.ts
+++ src/app/api/todo/route.ts
@@ -4,6 +4,14 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET( request: NextRequest){
 const client = await db.connect();
+
+
+try {
+    await client.sql `CREATE TABLE IF NOT EXISTS Todos(id serial, Task varchar(255));`
+} catch(err){
+console.log(err);
+return NextResponse.json({message: "Some thing went wrong"})
+
+}
     return NextResponse.json({234: "sad"})
 }
-//comment added
