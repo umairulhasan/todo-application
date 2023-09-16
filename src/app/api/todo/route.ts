eb6e7132729bf5bdf6fe1eab5293a95781f14aca
Umair <mrumairhassan@outlook.com>
2023-09-16 17:26:10 +0500

    try catch error remove and api run successfully

--- src/app/api/todo/route.ts
+++ src/app/api/todo/route.ts
@@ -8,10 +8,10 @@ const client = await db.connect();
 
 try {
     await client.sql `CREATE TABLE IF NOT EXISTS Todos(id serial, Task varchar(255));`
+    return NextResponse.json({message: "you call this api"})
 } catch(err){
 console.log(err);
 return NextResponse.json({message: "Some thing went wrong"})
 
 }
-    return NextResponse.json({234: "sad"})
 }
