d7a89653000a9d50668426747ec2c583e7a5f74c
Umair <mrumairhassan@outlook.com>
2023-11-01 21:34:12 +0500

    some update in get req and add find method

--- src/app/api/todo/route.ts
+++ src/app/api/todo/route.ts
@@ -7,8 +7,18 @@ export async function GET(request: NextRequest) {
 
 
     try {
+        //first query if first is already run then the skip this and go to next due to await 
         await client.sql`CREATE TABLE IF NOT EXISTS Todos(id serial, Task varchar(255));`
-        return NextResponse.json({ message: "you call this api" })
+        // secound query for get the data from todos table
+        const res = await client.sql`SELECT * FROM Todos`
+
+        // check response 
+
+        console.log(res)
+         // Filter the  or find use find method () or if u want loop
+
+         console.log(res.rows.find(items => items.id === 1))
+        return NextResponse.json({ message: res })
     } catch (err) {
         console.log(err);
         return NextResponse.json({ message: "Some thing went wrong" })
@@ -16,24 +26,21 @@ export async function GET(request: NextRequest) {
     }
 }
 
-
-
-
-export async function POST(request:NextRequest){
+export async function POST(request: NextRequest) {
 
     const client = await db.connect();
     const req = await request.json();
-try{
-if(req.task){
-await client.sql`INSERT INTO Todos(Task) VALUES(${req.task})`
-return NextResponse.json({message: "Data added successfully"})
+    try {
+        if (req.task) {
+            await client.sql`INSERT INTO Todos(Task) VALUES(${req.task})`
+            return NextResponse.json({ message: "Data added successfully" })
 
-}else{
-    throw new Error("TASK REQUIRED")
-}
-}
-catch(error){
-    console.log(error);
-    return NextResponse.json({ message: (error as {message : string}).message})
-}
+        } else {
+            throw new Error("TASK REQUIRED")
+        }
+    }
+    catch (error) {
+        console.log(error);
+        return NextResponse.json({ message: (error as { message: string }).message })
+    }
 }
